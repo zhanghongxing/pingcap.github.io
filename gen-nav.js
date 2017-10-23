ab94084ec315df2a33f020d40c5a374074914813
gaohailang <ghlndsl@126.com>
2017-10-23 17:26:34 +0800

    chore: 更新 docs 相关的 layout template，目录和首页用值逻辑

--- gen-nav.js
+++ gen-nav.js
@@ -4,54 +4,53 @@ const anchorTitleRe = /(\s*)[\+\-]\s*\[(.*)\]\((.*)\)/
 const titleRe = /^(\s*)[\+\-]\s*([^[]*)/
 
 function genTableJSONFromMarkdown(source, target, prefix) {
-
   const contents = fs.readFileSync(source, 'utf8')
   const reResult = tableRe.exec(contents)
-  let NavData = {children: []}
-  try{
+  let NavData = { children: [] }
+  try {
     const lines = reResult[0].split('\n')
     let level, lastLevel, parentNav, nav, lastParentNav, lastNav
 
-    lines.forEach((i, idx)=>{
+    lines.forEach((i, idx) => {
       let space, name, link
       const r1 = anchorTitleRe.exec(i)
       const r2 = titleRe.exec(i)
-      if(r1) {
+      if (r1) {
         space = r1[1]
         name = r1[2]
-        if((/^https?:\/\//).test(r1[3])) {
+        if (/^https?:\/\//.test(r1[3])) {
           link = r1[3]
         } else {
           link = prefix + r1[3].replace('.md', '/')
         }
-      } else if(r2) {
+      } else if (r2) {
         space = r2[1]
         name = r2[2]
       } else {
         return
       }
       level = space.length / 2
-      if(idx === 0) {
+      if (idx === 0) {
         lastLevel = 0
         level = 0
         lastParentNav = NavData
       }
-      nav = {name,link,}
-      if(level === lastLevel) {
+      nav = { name, link }
+      if (level === lastLevel) {
         nav.parent = lastParentNav
         lastParentNav.children.push(nav)
       }
-      if(level > lastLevel) {
-        if(!lastNav.children) {
+      if (level > lastLevel) {
+        if (!lastNav.children) {
           lastNav.children = []
         }
         nav.parent = lastNav
         lastNav.children.push(nav)
         lastParentNav = lastNav
       }
-      if(level < lastLevel) {
+      if (level < lastLevel) {
         let diff = lastLevel - level
-        while((--diff)>=0) {
+        while (--diff >= 0) {
           lastParentNav = lastParentNav.parent
         }
         nav.parent = lastParentNav
@@ -62,7 +61,7 @@ function genTableJSONFromMarkdown(source, target, prefix) {
       lastNav = nav
     })
     // clean/remove all parent key
-  }catch(e) {
+  } catch (e) {
     console.log('regexp markdown tables parse error')
   }
 
@@ -86,9 +85,10 @@ function genTableJSONFromMarkdown(source, target, prefix) {
   fs.writeFileSync(target, jsonTable, 'utf8')
 }
 
-
 var args = process.argv.slice(2)
-if(args.length != 3) {
-  console.log('Usage: node <this> arg1 arg2 arg3 # arg1 markdown source path, arg2 json file path arg3 url prefix')
+if (args.length != 3) {
+  console.log(
+    'Usage: node <this> arg1 arg2 arg3 # arg1 markdown source path, arg2 json file path arg3 url prefix'
+  )
 }
 genTableJSONFromMarkdown(args[0], args[1], args[2])
