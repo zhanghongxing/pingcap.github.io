9b5295dec0da240bc99130f9ec3018a5facde5b5
gaohailang <ghlndsl@126.com>
2017-11-23 16:32:57 +0800

    build: add circleci yml to ignore build in master branch

--- push-gh-pages.sh
+++ push-gh-pages.sh
@@ -10,5 +10,5 @@ git init
 git remote add origin "$remote"
 git checkout -b master
 git add .
-git commit -m "Update Release gh-pages"
+git commit -m "Update Release gh-pages[ci skip]"
 ssh-agent bash -c 'ssh-add ~/.ssh/id_rsa_6aa6ace89eee7def1892940bb02c3482;git push -f origin master'
