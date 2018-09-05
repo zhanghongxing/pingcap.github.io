e84a119e3d2c78d66ce6242f9bc028fec184ea8c
xuechunlei <xuechunlei922@gmail.com>
2018-09-06 00:52:06 +0800

    src/js/tidb-planet: fix authentiction logic

--- src/js/tidb_planet.js
+++ src/js/tidb_planet.js
@@ -3,62 +3,36 @@
 // TiDB Planet Pages
 
 const prefix = '_tidb_planet_'
-
 const cookiesKeyMap = {
-  AUTHENTICATED_CONTRIBUTORS_SUM: `${prefix}authenticated_contributors_sum`,
   CONTRIBUTOR_NUM: `${prefix}contributor_num`,
-  AUTHENTICATED_VISITORS_SUM: `${prefix}authenticated_visitors_sum`,
-  VISITOR_NUM: `${prefix}visitor_num`,
+  // VISITOR_NUM: `${prefix}visitor_num`,
+  USERNAME: `${prefix}username`,
 }
-
-let cookiesValMap = {}
 
 const getCookies = () => {
+  let cookiesValMap = {}
   for (let ck in cookiesKeyMap) {
-    console.log('cookie key', ck)
     const val = $.cookie(cookiesKeyMap[ck])
-    console.log('cookie value', val)
     cookiesValMap[ck] = val
   }
-
-  console.log('cookies', cookiesValMap)
+  return cookiesValMap
 }
 
-const isAuthContributor = () => {
-  console.log('is contributor', cookiesValMap['CONTRIBUTOR_NUM'])
-  return cookiesValMap['CONTRIBUTOR_NUM']
+const isAuthContributor = username => {
+  console.log('has username', username)
+  return window.tidbContributors[username]
 }
 
-const isAuthVisitor = () => {
-  console.log('is visitor', cookiesValMap['VISITOR_NUM'])
-  return cookiesValMap['VISITOR_NUM']
-}
-
-const authenticateContributor = user => {
-  getCookies()
-
-  if (window.tidbContributors[user]) {
-    // auth success: is contributor
-    const cSum = +cookiesValMap['AUTHENTICATED_CONTRIBUTORS_SUM'] || 0
-    const cNum = cSum + 1
-    console.log(
-      `Congratulations! You are the ${cNum}th landing on TiDB Planet!`
-    )
-    // update AUTHENTICATED_CONTRIBUTORS_SUM and CONTRIBUTOR_NUM： both +1
-    $.cookie(cookiesKeyMap['AUTHENTICATED_CONTRIBUTORS_SUM'], cNum)
-    $.cookie(cookiesKeyMap['CONTRIBUTOR_NUM'], cNum)
+const authenticateContributor = name => {
+  if (window.tidbContributors[name]) {
+    // auth success: is a contributor
+    // TODO: change algorithm to getting contributor number
+    // sort by commit date
+    // update contributor number
   } else {
-    // auth failed: is visitor
-    console.log(
-      'AUTHENTICATED_VISITORS_SUM',
-      +cookiesValMap['AUTHENTICATED_VISITORS_SUM']
-    )
-    const vSum = +cookiesValMap['AUTHENTICATED_VISITORS_SUM'] || 0
-    const vNum = vSum + 1
-    console.log(`Welcome to the TiDB planet! You are the ${vNum}th visitor!`)
-    // update AUTHENTICATED_VISITORS_SUM and VISITOR_NUM
-    $.cookie(cookiesKeyMap['AUTHENTICATED_VISITORS_SUM'], vNum)
-    $.cookie(cookiesKeyMap['VISITOR_NUM'], vNum)
+    // auth failed: is a visitor
+    console.log('Not a contributor')
+    // TODO: visitor card number
   }
 }
 
@@ -68,20 +42,11 @@ $(function() {
     window.tidbContributors = $('#tidb-planet').data('contributors')
   }
 
-  // get cookies and
-  getCookies()
-
-  // judge user identity
-  if (isAuthContributor()) {
-    console.log(
-      `Congratulations! You are the ${
-        cookiesValMap['CONTRIBUTOR_NUM']
-      }th landing on TiDB Planet!`
-    )
-  } else if (isAuthVisitor()) {
-    console.log('Welcome to the TiDB planet, join us now! www.pingcap.com')
-  } else {
-    // new user
+  // get username
+  const username = getCookies()['USERNAME']
+  console.log('username in cookies', username)
+  if (!username) {
+    // is a new user
     // TODO: play milestones video
     console.log('You are fresh~ please login')
     // TODO: after play video, show login mask
@@ -90,16 +55,19 @@ $(function() {
     // TODO: login input
     // after input username
     // TODO: authenticate contributor
-    // test user
-    const user = 'liubin'
-    authenticateContributor(user)
+    // TODO: test user
+    const inputName = 'xuechunL'
+    authenticateContributor(inputName)
+    $.cookie(cookiesKeyMap['USERNAME'], inputName)
+  } else if (isAuthContributor(username)) {
+    // is a contributor
+    console.log(
+      `Congratulations! You are the ${
+        getCookies()['CONTRIBUTOR_NUM']
+      }th landing on TiDB Planet!`
+    )
+  } else {
+    // is a visitor
+    console.log('Welcome to the TiDB planet, join us now! www.pingcap.com')
   }
-  // ajax get contributors
-  // $.ajax({
-  //   url: 'https://api.github.com/repos/pingcap/tidb/contributors',
-  //   crossDomain: true,
-  //   success: function(data) {
-  //     console.log(data)
-  //   },
-  // })
 })
