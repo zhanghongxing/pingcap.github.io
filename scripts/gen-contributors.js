e0cdbd8f78b2532c44453cdff0a69e130287fe4a
xuechun Lei <xuechunlei922@gmail.com>
2018-09-14 21:41:45 +0800

    *: update generate contributors logic and update new contributors.json

--- scripts/gen-contributors.js
+++ scripts/gen-contributors.js
@@ -1,40 +1,24 @@
 // generate contributors.json for tidb-planet pages
-const Octokit = require('@octokit/rest')
-const octokit = Octokit()
-const octokit2 = Octokit()
-// const octokit = require('@octokit/rest')()
+const octokit = require('@octokit/rest')()
 // https://github.com/octokit/rest.js
 
-
 octokit.authenticate({
-  type: 'token',
-  token: '4e7ff6b3340782cabce2f2e87b2b970843bab063'
+  type: 'oauth',
+  key: '4d00056f425d3297d24f',
+  secret: '1c2b099ceb13dd845507f87b600b28afa16894dc'
 })
-
-// use new token
-octokit2.authenticate({
-  type: 'token',
-  token: '37abbd44c6a17a5c04111b599e616b76a2f7be58',
-})
-
-
-
-
-
 
 const fs = require('fs')
 const _ = require('lodash')
 
-const repos = ['docs', 'docs-cn', 'tidb', 'tikv', 'pd', 'tispark', 'tidb-operator']
-// const repos = ['docs', 'docs-cn', 'tispark', 'tidb-operator']
-
+const repos = ['tidb', 'tikv', 'pd', 'tispark', 'docs', 'docs-cn', 'tidb-ansible', 'tidb-operator', 'rust-rocksdb', 'raft-rs', 'grpc-rs', 'rust-prometheus']
 const owner = 'pingcap'
 const per_page = 100
 
-// FIXME: GitHub REST API Issue: empty commit list
-const omittedContributorsCommits = {
-  liubin: { first_commit: { date: "2016-04-26", repo: 'tidb' } },
-  buggithubs: { first_commit: {date: "2017-11-20", repo: 'tidb'}}
+const delay = (interval) => {
+  return new Promise((resolve) => {
+      setTimeout(resolve, interval);
+  });
 }
 
 // fetch TiDB repository contributors
@@ -43,8 +27,10 @@ async function getRepoContributors(repo) {
   const result = await octokit.repos.getContributors({
     owner,
     repo,
-    per_page})
-  await delay(1000)
+    per_page
+  })
+
+  // await delay(2000)
   await inner(result)
   return all
 
@@ -55,88 +41,38 @@ async function getRepoContributors(repo) {
       let _r = await octokit.getNextPage(result)
       await inner(_r)
     } else {
-      console.log(all.length)
+      console.log(`repo ${repo} contributors: ${all.length}`)
     }
   }
 }
 
-
-// generate each contributor's first commit
-async function genContributorFirstCommit(repo, author) {
-  let first_commit = {}
-  // use another octokit
-  await delay(2000)
-  const result = await octokit2.repos.getCommits({owner, repo, author, per_page})
-  await innerCallback(result)
-  return first_commit
-
-  async function innerCallback(result) {
-    let _res = result
-    if(octokit.hasLastPage(result))
-      _res = await octokit.getLastPage(result)
-
-    if(_res.data.length) {
-      const commit = _res.data[_res.data.length - 1].commit
-      if(commit)
-        first_commit = { date: commit.author.date, repo }
-    }
-  }
+// generate each repository's created date
+async function genRepoCreatedTime(repo) {
+  // await delay(2000)
+  const result = await octokit.repos.get({owner, repo})
+  const { created_at } = result.data
+  return created_at
 }
-
-
-// get first commit date for each contributor
-// https://blog.lavrton.com/javascript-loops-how-to-handle-async-await-6252dd3c795
-async function processContributorsCommits (repo, list) {
-  // map array to promises
-  const promises = list.map(asyncGenCommit)
-  // wait until all promises are resolved
-  let commits = {}
-  await Promise.all(promises)
-  return commits
-
-  async function asyncGenCommit(c) {
-    const { login } = c
-    const first_commit = await genContributorFirstCommit(repo, login) || ''
-    if(!commits[login]) {
-      commits[login] = first_commit
-    }
-  }
-}
-
-const delay = (interval) => {
-  return new Promise((resolve) => {
-      setTimeout(resolve, interval);
-  });
-};
 
 async function main() {
-  const result = await octokit.misc.getRateLimit({})
-  const result2 = await octokit2.misc.getRateLimit({})
-  console.log('rate limit', result)
-  console.log('rate limit 2', result2)
-
+  // const result = await octokit.misc.getRateLimit({})
+  // console.log(result)
   let contributors = {}
 
   for(let r of repos) {
-    let commits= {}
-    // contributorList = _.concat(contributorList, await getRepoContributors(i))
     const specRepoContributors = await getRepoContributors(r)
-    console.log(`repo ${r} contributors: ${specRepoContributors.length}`)
-    commits = await processContributorsCommits(r, specRepoContributors)
-    // FIXME: add omitted contributors in tidb repo
-    if(r === 'tidb')
-      commits = Object.assign({}, commits, omittedContributorsCommits)
-    console.log(`repo ${r} commits: ${commits}`)
-    await genJSON(specRepoContributors, commits)
-    await delay(5000)
+    const created_at = await genRepoCreatedTime(r)
+    await genJSON(specRepoContributors, created_at)
   }
 
-  console.log('all contributors', contributors)
+  const sortedList = _.sortBy(_.toArray(contributors), c => c.contributions)
+  console.log('number of all contributors', sortedList.length)
+
   // write to file
-  fs.writeFileSync('data/contributors.json', JSON.stringify(contributors), 'utf8')
+  fs.writeFileSync('data/contributors.json', JSON.stringify(_.reverse(sortedList)), 'utf8')
 
   // generate contributors.json file
-  async function genJSON(specRepoContributors, commits) {
+  async function genJSON(specRepoContributors, date) {
     specRepoContributors.forEach(c => {
       const { login, avatar_url, contributions } = c
 
@@ -145,12 +81,10 @@ async function main() {
           login,
           avatar_url,
           contributions,
-          first_commit: commits[login]
+          date
         }
       } else {
-        const { date } = commits[login]
-        if(date < contributors[login].first_commit.date)
-          contributors[login].first_commit = commits[login]
+        contributors[login].contributions += contributions
       }
     })
   }
